a9da9f90cac2613ff461f613ea0009a2277a89e0
Gulácsi András <guland@protonmail.com>
2023-11-03 23:07:21 +0100

    delete my custom themes in src folder (will be replaced with a default wp theme)

--- src/wp-content/themes/decimus/inc/components.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once get_template_directory() . '/inc/components/bs-share-buttons/main.php';
-require_once get_template_directory() . '/inc/components/bs5-post-page-grid-list/main.php';
-require_once get_template_directory() . '/inc/components/bs5-contact-form-7/main.php';
--- src/wp-content/themes/decimus/searchform.php
+++
@@ -1,5 +0,0 @@
-<!-- Search Button Outline Secondary Right -->
-<form class="searchform input-group" method="get" action="<?php echo esc_url(home_url('/')); ?>" class="form-inline">
-    <input type="text" name="s" class="form-control" placeholder="<?php _e('Search', 'decimus'); ?>">
-    <button type="submit" class="input-group-text btn btn-secondary"><i class="fas fa-search"></i></button>
-</form>
